0cd1771d266e11203fa594288eedf6f2be9202c7
Lander Álvarez S <vealvarezs@gmail.com>
2023-04-07 10:11:44 -0400

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,33 @@
-import logo from './logo.svg';
+
 import './App.css';
+import Testimonio  from './componentes/Testimonio';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className='contenedor-principal'>
+      <h1>Galería de Imagenes con React</h1>
+      <Testimonio 
+      nombre='El avance de la programación'
+      imagen='code' 
+      testimonio='Cada vez son más los accesos para aprender programación a nivel usuario.'/> 
+
+      <Testimonio
+      nombre='El futuro de la AI'
+      imagen='robot'
+      testimonio='Proximamente la AI se verá reflejada en asistentes robots altamente eficientes.'/>
+
+      <Testimonio
+       nombre='Las nuevas tecnologias'
+       imagen='tecno'
+       testimonio='El avance de las pantallas flexibles, los hologramas y la AI, auspician una revolución tecnologica.'/>
+
+<Testimonio
+       nombre='VR una herramienta que quiere alcanzar su maximo potencial'
+       imagen='vr'
+       testimonio='Nuevos usos en la tecnologia de Virtual Reality en el area de la medicina.'/>
+ 
+      </div>
     </div>
   );
 }
